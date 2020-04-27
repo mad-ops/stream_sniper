cfa568383c9e5c8923e0d7de7f6868448557378d
mad-ops <vish@mad-ops.com>
2020-04-27 17:28:04 -0500

    Move file is there is nothing to capture

--- sniper.py
+++ sniper.py
@@ -12,6 +12,10 @@ parser.add_argument('streamer', metavar='u/n', help='username, found in TTV URL'
 args = parser.parse_args()
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))
+os.makedirs(os.path.join(scriptdir, "Videos", "in_progress"), exist_ok=True)
+os.makedirs(os.path.join(scriptdir, "Videos", "encode_ready"), exist_ok=True)
+encode_path = os.path.join(scriptdir, "Videos", "encode_ready")
+capture_path = os.path.join(scriptdir, "Videos", "in_progress")
 
 def findUrl( streamer ):
     # Check for Livestream
@@ -27,6 +31,17 @@ def findUrl( streamer ):
 
     return streams["best"]
 
+def file_list( path ,  type ):
+    onlyfiles = [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]  
+    return list(filter(lambda x: x.endswith(type) , onlyfiles))
+
+def ship_ts( streamer ):
+    #should only return 1
+    vods = [f for f in file_list(capture_path, ".ts") if streamer in f]
+    pretty_time = time.strftime("%Y-%m-%d-%H")
+    [os.rename(vod, os.path.join(encode_path, f"{args.streamer}_{pretty_time}.ts") ) for vod in vods]
+    return
+
 def main( args ):
     url = findUrl(args.streamer)
 
@@ -34,21 +49,21 @@ def main( args ):
     # I should also confirm the size of a frame
     chunkSize = 1920 * 1080 *  3  # read length*width*3 bytes (= 1 frame)
 
-    # lets only run for 30 minutes at a time -- cause im scared
+    # lets only run for 15 minutes at a time -- cause im scared
     timeout_start = time.time()
-    pretty_time = time.strftime("%Y-%m-%d-%H:%M")
-    timeout = 30 * 60
+    timeout = 15 * 60
     
     try:
         fd = url.open()
     except:
         print("Could not open stream.")
+        #Move file
+        ship_ts(args.streamer)
         return
     
-    file_name = os.path.join(scriptdir, "Videos", f"{args.streamer}__{pretty_time}.ts")
-    os.makedirs(os.path.join(scriptdir, "Videos"), exist_ok=True)
+    file_name = os.path.join(scriptdir, "Videos", "in_progress", f"{args.streamer}.ts")
     
-    with open(file_name, "wb") as ts:
+    with open(file_name, "ab") as ts:
         while time.time() < timeout_start + timeout:
             data = fd.read(chunkSize)
             ts.write(data)
